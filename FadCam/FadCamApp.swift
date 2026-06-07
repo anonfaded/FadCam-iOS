770196ff36a4472cde41563d059d11cb0f256084
Faded <anonfaded@pm.me>
2026-06-07 10:04:30 +0500

    hello world

---
+++ FadCam/FadCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FadCamApp.swift
+//  FadCam
+//
+//  Created by FADED on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct FadCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
